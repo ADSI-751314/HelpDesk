25b884498eb63524a2ca34c2429b38a9853c7af0
☼JuanK © » Andron☼ <Juankabuitrago@gmail.com>
2015-08-31 19:53:28 -0500

    Instalacion de la estructura del proyecto.

---
+++ model/reportes_class.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of reportes_class
+ *
+ * @author Juan-Buitrago
+ */
+class reportes_class {
+    //put your code here
+}
