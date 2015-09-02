889e6d428899998afd2e6a9305a2dd325eb05aad
☼JuanK © » Andron☼ <Juankabuitrago@gmail.com>
2015-09-01 19:18:01 -0500

    Estructura Basica HTML del proyecto Se crea una interfaz basica del proyecto para poder trabajar con los nuevos formularios.

--- index.php
+++ index.php
@@ -1,8 +1,35 @@
-<?php
-
-/* 
+<!--/* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Mesa de Ayuda</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="view/css/layout.css">
+        <script type="application/javascript" language="javascript" src="view/js/jquery.js"></script>
+    </head>
+    <body>
+        <div class="logo"></div>
 
+        <div class="content">
+            <div class="menu">
+                <nav>
+                    <ul>
+                        <li><a href="">Reportes</a></li>
+                        <li><a href="">Inventarios</a></li>
+                        <li><a href="">Ticket</a></li>
+                    </ul>
+                </nav>
+
+            </div>
+            <h1>Contenido del proyecto</h1>
+        </div>    
+        <?php
+        ?>
+    </body>
+</html>
