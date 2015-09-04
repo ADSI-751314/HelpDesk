dd5b836b216efa0209a47c610ad16ef791a786e5
Sena <Sena@10.17.18.225>
2015-09-03 19:54:43 -0500

    creacion del formulario Reportes

--- view/forms/frm_reportes.php
+++ view/forms/frm_reportes.php
@@ -1,8 +1,57 @@
-<?php
+<!DOCTYPE html>
+<html>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    <head>
+
+        <title> MODULO REPORTES </title>
+    </head>
+
+    <body>
+
+
+        <form name="frmReportes " >
+            <table border="1px">
+                <tr >                   
+
+                    <td colspan="2" >REPORTES  </td>
+                    <td colspan="2" >
+                        <select name="LISTA">
+                            <option disabled="disabled" selected="selected">SELECCIONE---></option>
+                            <option>FALLAS MAS COMUNES</option>
+                            <option>USUARIOS MAS CRITICOS</option>
+                            <option>PARTES QUE MAS FALLAN</option>
+                            <option>MARCAS QUE MAS FALLAN</option>
+                        </select>
+                    </td>
+                <br>             
+                </tr>
+                <tr>
+
+                </tr>
+
+                <tr>   
+                    <td colspan="2"> TICKET  </td>
+                    <td colspan="2"><input type="texto" name="txtticket" size="18" maxlength="10"></td>
+
+                </tr>
+                <tr>  
+                    <td>FECHA INICIO </td>
+                    <td><input type="date" name="FechaInicio"> </td>
+                    <td>FECHA FIN </td>
+                    <td><input type="date" name="FechaFin"> </td>
+
+                <tr>    <td> <input type="submit" name="btnConsultar" value="Consultar"  class="boton"></td>         </tr>
+
+
+
+
+            </table>            
+
+
+
+
+        </form>
+
+    </body>
+</html>
 
