f4abed3ac5d38100caba799e5b74c8f221659be3
metalnando <hmoreno1987@hotmail.com>
2015-09-03 23:24:56 -0500

    Se modifica la forma de ingreso de las fechas en el formulario

--- view/forms/frmSolicitudServicio.php
+++ view/forms/frmSolicitudServicio.php
@@ -7,49 +7,6 @@
 	<title>Solicitud de Servicios</title>
 </head>
 <link rel="stylesheet" type="text/css" href="estilo.css" />
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.min.js"></script>
-<script type="text/javascript">
-jQuery(function($){
-	$.datepicker.regional['es'] = {
-		closeText: 'Cerrar',
-		prevText: '&#x3c;Ant',
-		nextText: 'Sig&#x3e;',
-		currentText: 'Hoy',
-		monthNames: ['Enero','Febrero','Marzo','Abril','Mayo','Junio',
-		'Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
-		monthNamesShort: ['Ene','Feb','Mar','Abr','May','Jun',
-		'Jul','Ago','Sep','Oct','Nov','Dic'],
-		dayNames: ['Domingo','Lunes','Martes','Mi&eacute;rcoles','Jueves','Viernes','S&aacute;bado'],
-		dayNamesShort: ['Dom','Lun','Mar','Mi&eacute;','Juv','Vie','S&aacute;b'],
-		dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','S&aacute;'],
-		weekHeader: 'Sm',
-		dateFormat: 'dd/mm/yy',
-		firstDay: 1,
-		isRTL: false,
-		showMonthAfterYear: false,
-		yearSuffix: ''};
-	$.datepicker.setDefaults($.datepicker.regional['es']);
-});    
- 
-$(document).ready(function() {
-   $("#datepicker").datepicker();
- });
-
-$(document).ready(function(){
-   $("#datepicker").datepicker({
-    showAnim: 'fadeIn'
-    });
-    
-    $(document).ready(function(){
-  $("#datepicker").datepicker({ 
-       showOn: 'button', 
-       buttonText: "Seleccionar" });
- });
-});
-
-</script>
-
 <body>
 <form>
 <br />
@@ -72,7 +29,7 @@ $(document).ready(function(){
     
     <tr>
      <td>Fecha: </td>
-     <td><input type="text" name="datepicker" id="datepicker" readonly="readonly" size="12" /></td>
+     <td><input type="date" name="fecha" /></td>
      <td>Hora: </td>
      <td> <input type="text" maxlength="25" name="txtEstado" /> </td>
      <td>Prioridad: </td>
@@ -90,7 +47,7 @@ $(document).ready(function(){
      <td>Tipo Equipo: </td>
      <td><input type="text" name="txtTipoEquipo" /></td>
      <td>Fecha Actualizaci&oacute;n</td>
-     <td><input type="text" name="datepicker" id="datepicker" readonly="readonly" size="12" /></td>
+     <td><input type="date" name="fecha"></td>
     </tr>
    
     <tr>
