af8cc20e5c252a47f0b0e93c1c895c6a5b3d3516
sena <sena@10.17.19.37>
2015-09-04 18:54:59 -0500

    Se hace actualizacion del formulario

--- view/forms/frm_base_conocimiento.php
+++ view/forms/frm_base_conocimiento.php
@@ -1,40 +1,63 @@
-
 <html>
-    <head>
-        <title> Formulario Buscar </title>
-    </head>
-    <body>
-        <form name="frmBuscar.php">
-            <table class="fondo" align="center">
+<head>
+<title>BASE DE CONOCIMIENTO</title>
+<link rel="stylesheet" href="body2.css">
+<script type="text/javascript"> 
+ 
+</head>
+<body bgColor="#FA8072">
+<div id="nav" align="top" ><br>
+<button type="submit" name="Nuevo" >Nuevo Registro</button>
+<button type="submit" name="Actualizar">Actualizar</button>	
+<button type="submit" name="Consultar">Consultar</button>
+			
+</div>
+<table align="middle" class="texto">
+<tr>
+<td colspan="3" align="middle" class="texto"> BASE DE CONOCIMIENTO </td>
+</table><br>	
+</tr>
 
-                <tr>
-                    <td colspan="2" align="center" class="textos"> Base de conocimiento </td>
-                </tr>
-                <tr> 
-                    <td class="textos"> Buscar por Balabra Clave </td>
-                    <td><input type="text"name="txtBuscar_Clave" size="50"maxlength="100"></td>
-                </tr>
-                <tr>
-                    <td class="textos"> Busqueda Especifica </td>
-                    <td> <input type="text"name="txtBuscar_especifica" size="50"maxlength="100"></td>
-                </tr>
-                <tr>
-                    <td class="textos"> Descripcion de la Falla </td>
-                    <td> <textarea id= "textarea" name="descripion_falla" rows="10"cols="50"> </textarea><br><br></td>
-                </tr>
-                <tr>
-                    <td class="textos"> Solucion de la Falla </td>
-                    <td> <textarea id= "textarea" name="Solucion_falla" rows="10"cols="50"> </textarea><br><br></td>
-                </tr>
-                <tr align="center">
-                    <td colspan="4">
-                        <input type="submit" name="btnBuscar" size="10" value="Buscar" class="textos" cellpadding="center"><br><br><br>
-                        <input type="submit" name="btnNuevo" size="10" value="Nuevo" class="textos"> 
-                        <input type="submit" name="btnActualizar" size="10" value="Actualizar" class="textos"> 
-                        <input type="submit" name="btnDescripcion_Falla" size="20" value="Descripcion_Falla" class="textos"> 
-                        <input type="submit" name="btnSolucion_Falla" size="10" value="Solucion_Falla" class="textos"> 
-            </table>
-        </form>
-    </body>
+
+<input type="image" name="GNU" alt="GNU" src="ADSI.jpg" align="right"/>
+
+
+<form name="frmBuscar.php"><br><br>
+			<table class="fondo" align="center">
+			
+			
+			<tr> 
+				<td class="textos"> Adicionar Balabra Clave </td>
+				<td><form id="buscador" name="buscador" method="post" size="10"action="<?php echo $_SERVER['PHP_SELF'] ?>">  
+				<input id="buscar" name="buscar" type="search" placeholder="Buscar aqui..." autofocus > 
+				<input type="submit" name="buscador" class="boton peque aceptar" size="20" value="Buscar" > </form> </td>
+			
+			</tr>
+			<tr>
+				<td class="textos"> Adicionar Busqueda Especifica </td>
+				<td> <input type="text"name="txtBuscar_especifica" size="50"maxlength="100"><br></td></td>
+			
+			</tr>
+			<tr>
+				<td class="textos"> Descripcion de la Falla </td>
+				<td> <textarea id= "textarea" name="descripion_falla" rows="10"cols="50"> </textarea><br><br></td>
+			
+			</tr>
+			<tr>
+				<td class="textos"> Solucion de la Falla </td>
+				<td> <textarea id= "textarea" name="Solucion_falla" rows="10"cols="50"> </textarea><br><br></td>
+			
+			</tr>
+			<tr align="center">
+				
+				<td colspan="4">
+				<input type="submit" name="btnCrear" size="10" value="Crear" class="textos" cellpadding="center"><br><br><br>
+				
+				
+				
+				
+			</table>
+	</form>
+</body>
+
 </html>
-
