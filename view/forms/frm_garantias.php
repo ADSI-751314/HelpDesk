988783fe148619b0c02600d5be623bcdff59a404
carloseduardo <carloseduardo@10.17.16.214>
2015-09-04 20:48:50 -0500

    formulario garantias listo. hay que hacerles. siguiente conexiones

--- view/forms/frm_garantias.php
+++ view/forms/frm_garantias.php
@@ -1,25 +1,26 @@
         <form>
-            <table align="center">
+            <br>
+            <table>
                 <tr>
-                    <td> Garantía Número: </td>  <td> <input type="text" id="NoGarantia" size="40" maxlength="10"> </td>
+                    <td> Garantía Número: </td>  <td> <input type="text" id="NoGarantia" size="30" maxlength="10"> </td>
                 </tr>
                 <tr>
-                    <td> Tipo de garantía:  </td>   <td> <input type="radio" id="hardware" name="Hardware" value="hardware"> Hardware <input type="radio" id="software" name="Software" value="software"> Software </td>
+                    <td> Tipo de garantía:  </td>   <td align="left"><input type="radio" id="hardware" name="Hardware" value="hardware"> Hardware <input type="radio" id="software" name="Software" value="software"> Software </td>
                 </tr>
                 <tr>
-                    <td> Fecha de inicio: </td>      <td><input type="date" name="fecha"></td>
+                    <td> Fecha de inicio: </td>      <td align="left"><input type="date" name="fecha"></td>
                 </tr>
                 <tr>
-                    <td>Fecha fin de garantía:</td> <td><input type="date" name="fecha2"></td>
+                    <td>Fecha fin de garantía:</td> <td align="left"><input type="date" name="fecha2"></td>
                 </tr>
                 <tr>
                     <td>Descripción: </td> <td> <textarea cols="30" rows="20"></textarea></td>
                 </tr>
                 <tr>
-                    <td> <input type="submit" name="btnGuardar" value="GUARDAR"> </td> 
-                    <td> <input type="submit" name="btnModificar" value="MODIFICAR"> </td> 
-                    <td> <input type="submit" name="btnConsultar" value="CONSULTAR"> </td>
-                    <td> <input type="submit" name="btnEliminar" value="ELIMINAR"> </td>
+                    <td colspan="2"> <input type="submit" name="btnGuardar" value="GUARDAR"> 
+                     <input type="submit" name="btnModificar" value="MODIFICAR"> 
+                    <input type="submit" name="btnConsultar" value="CONSULTAR"> 
+                     <input type="submit" name="btnEliminar" value="ELIMINAR"> </td>
                     
                 </tr>
             </table>
