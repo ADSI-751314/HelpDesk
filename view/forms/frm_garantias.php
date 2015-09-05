bf47221863df783ff21c601aaae174d916ca7986
carloseduardo <carloseduardo@10.17.16.214>
2015-09-04 21:11:00 -0500

    ahi vamos ;)

--- view/forms/frm_garantias.php
+++ view/forms/frm_garantias.php
@@ -2,10 +2,10 @@
             <br>
             <table>
                 <tr>
-                    <td> Garantía Número: </td>  <td> <input type="text" id="NoGarantia" size="30" maxlength="10"> </td>
+                    <td> Garantía Número: </td>  <td align="left"><input type="text" name="txtGarantiaID" id="numero_arantia" size="12" maxlength="12"> </td>
                 </tr>
                 <tr>
-                    <td> Tipo de garantía:  </td>   <td align="left"><input type="radio" id="hardware" name="Hardware" value="hardware"> Hardware <input type="radio" id="software" name="Software" value="software"> Software </td>
+                    <td> Tipo de garantía:  </td>   <td align="left"><input type="radio" id="hardware" name="hardware" value="hardware"> Hardware <input type="radio" id="software" name="software" value="software"> Software </td>
                 </tr>
                 <tr>
                     <td> Fecha de inicio: </td>      <td align="left"><input type="date" name="fecha"></td>
@@ -14,7 +14,7 @@
                     <td>Fecha fin de garantía:</td> <td align="left"><input type="date" name="fecha2"></td>
                 </tr>
                 <tr>
-                    <td>Descripción: </td> <td> <textarea cols="30" rows="20"></textarea></td>
+                    <td>Descripción: </td> <td> <textarea cols="50" rows="15" name="txtDescripcion" id="descripcion"></textarea></td>
                 </tr>
                 <tr>
                     <td colspan="2"> <input type="submit" name="btnGuardar" value="GUARDAR"> 
