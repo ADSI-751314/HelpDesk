ea82ce7b84c1726b69c442961b412a93633c0165
☼JuanK © » Andron☼ <Juankabuitrago@gmail.com>
2015-09-04 19:19:49 -0500

    Incorporacion del favicon

--- view/forms/head.php
+++ view/forms/head.php
@@ -5,6 +5,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="view/css/general.css">
+        <link rel="shortcut icon" href="view/img/favicon.png">
         <script type="application/javascript" language="javascript" src="view/js/jquery.js"></script>
         <script type="text/javascript" src="view/js/general.js"></script>
     </head>
