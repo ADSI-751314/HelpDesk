30523745dbf4f9416bf8048c155068ff83c5e6f1
☼JuanK © » Andron☼ <Juankabuitrago@gmail.com>
2015-09-04 19:30:07 -0500

    Correcion de errores menores se corrigen errores en las etiquetas html de los formularios

--- view/forms/frm_base_conocimiento.php
+++ view/forms/frm_base_conocimiento.php
@@ -1,63 +1,30 @@
-<html>
-<head>
-<title>BASE DE CONOCIMIENTO</title>
-<link rel="stylesheet" href="body2.css">
-<script type="text/javascript"></script> 
- 
-</head>
-<body bgColor="#FA8072">
-<div id="nav" align="top" ><br>
-<button type="submit" name="Nuevo" >Nuevo Registro</button>
-<button type="submit" name="Actualizar">Actualizar</button>	
-<button type="submit" name="Consultar">Consultar</button>
-			
-</div>
-<table align="middle" class="texto">
-<tr>
-<td colspan="3" align="middle" class="texto"> BASE DE CONOCIMIENTO </td>
-</table><br>	
-</tr>
-
-
-<input type="image" name="GNU" alt="GNU" src="ADSI.jpg" align="right"/>
-
-
 <form name="frmBuscar.php"><br><br>
-			<table class="fondo" align="center">
-			
-			
-			<tr> 
-				<td class="textos"> Adicionar Balabra Clave </td>
-				<td><form id="buscador" name="buscador" method="post" size="10"action="<?php echo $_SERVER['PHP_SELF'] ?>">  
-				<input id="buscar" name="buscar" type="search" placeholder="Buscar aqui..." autofocus > 
-				<input type="submit" name="buscador" class="boton peque aceptar" size="20" value="Buscar" > </form> </td>
-			
-			</tr>
-			<tr>
-				<td class="textos"> Adicionar Busqueda Especifica </td>
-				<td> <input type="text"name="txtBuscar_especifica" size="50"maxlength="100"><br></td></td>
-			
-			</tr>
-			<tr>
-				<td class="textos"> Descripcion de la Falla </td>
-				<td> <textarea id= "textarea" name="descripion_falla" rows="10"cols="50"> </textarea><br><br></td>
-			
-			</tr>
-			<tr>
-				<td class="textos"> Solucion de la Falla </td>
-				<td> <textarea id= "textarea" name="Solucion_falla" rows="10"cols="50"> </textarea><br><br></td>
-			
-			</tr>
-			<tr align="center">
-				
-				<td colspan="4">
-				<input type="submit" name="btnCrear" size="10" value="Crear" class="textos" cellpadding="center"><br><br><br>
-				
-				
-				
-				
-			</table>
-	</form>
-</body>
+    <table class="fondo" align="center">
+        <tr> 
+            <td class="textos"> Adicionar Balabra Clave </td>
+            <td><form id="buscador" name="buscador" method="post" size="10"action="<?php echo $_SERVER['PHP_SELF'] ?>">  
+                    <input id="buscar" name="buscar" type="search" placeholder="Buscar aqui..." autofocus > 
+                    <input type="submit" name="buscador" class="boton peque aceptar" size="20" value="Buscar" > </form> </td>
+        </tr>
+        <tr>
+            <td class="textos"> Adicionar Busqueda Especifica </td>
+            <td> <input type="text"name="txtBuscar_especifica" size="50"maxlength="100"><br></td></td>
 
-</html>
+        </tr>
+        <tr>
+            <td class="textos"> Descripcion de la Falla </td>
+            <td> <textarea id= "textarea" name="descripion_falla" rows="10"cols="50"> </textarea><br><br></td>
+
+        </tr>
+        <tr>
+            <td class="textos"> Solucion de la Falla </td>
+            <td> <textarea id= "textarea" name="Solucion_falla" rows="10"cols="50"> </textarea><br><br></td>
+
+        </tr>
+        <tr align="center">
+
+            <td colspan="4">
+                <input type="submit" name="btnCrear" size="10" value="Crear" class="textos" cellpadding="center"><br><br><br>
+
+    </table>
+</form>
---
+++ view/forms/frm_solicitud_servicio.php
@@ -0,0 +1,50 @@
+<table border="1" align="center" class="fondo">
+    <th colspan="6" >Solicitud de Servicios</th>
+    <tr>
+        <td>ID Ticket: </td>
+        <td> <input type="text" maxlength="25" name="txtTicket" /></td>
+        <td>Usuario:</td>
+        <td><input type="text" maxlength="25" name="txtUsuario" /></td>
+        <td>Estado: </td>
+        <td> <input type="text" maxlength="25" name="txtEstado" /></td>
+    </tr>
+    <tr>
+        <td>Fecha: </td>
+        <td><input type="date" name="fecha" /></td>
+        <td>Hora: </td>
+        <td> <input type="text" maxlength="25" name="txtEstado" /> </td>
+        <td>Prioridad: </td>
+        <td><select name="selCombo" size="1" > 
+                <option value=" ">Cr&iacute;tico</option>
+                <option value=" ">Alta</option>
+                <option value=" ">Normal</option>
+                <option value=" ">Baja</option> 
+            </select></td>
+    </tr>
+    <tr>
+        <td>ID Equipo: </td>
+        <td><input type="text" name="txtIdEquipo" /></td>
+        <td>Tipo Equipo: </td>
+        <td><input type="text" name="txtTipoEquipo" /></td>
+        <td>Fecha Actualizaci&oacute;n</td>
+        <td><input type="date" name="fecha"></td>
+    </tr>
+    <tr>
+        <td> Desccripci&oacute;n <center></center> </td>
+<td colspan="5"> <table>
+        <tr ><textarea rows="8" cols="100"> </textarea> </tr>
+    </table></td>
+</tr>
+
+<tr>
+    <td colspan="6" align="center"> <input type="submit" name="btnEnviar" value="ENVIAR" /> </td>
+
+</tr>
+
+</table>
+
+</form>
+
+
+</body>
+</html>
