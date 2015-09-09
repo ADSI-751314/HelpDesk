34e1dc4e7cbe00175a8e891990dbcfa9be3db7f6
SENA <SENA@10.17.22.134>
2015-09-08 19:11:58 -0500

    Sin html

--- view/forms/frmPoliticas.php
+++ view/forms/frmPoliticas.php
@@ -1,36 +1,36 @@
-<html>
-<head>
-    
-</head>
-	<body>
-		<form name="frmPoliticas">
-			<table border="1" >
-				<tr>
-					<td colspan="4" class="titulos" align="center"> POLITICAS </td>					
-				</tr>
-				<tr>
-					<td> CODIGO POLITICAS: </td>
-					<td> <input type="text" name="txtPoliticas" size="8" maxlength="6"> </td>
-				</tr>
-				<tr>
-					<td> DESCRIPCION: </td>
-					<td> <input type="text" name="txtDescripcion" size="60" maxlength="4"> </td>
-				
-				</tr>
-				<tr>
-				<td colspan="4" align="center">
-						<input type="button" name="btnGuardar" value="Guardar">
-						<input type="button" name="btnModificar" value="Modificar">
-						<input type="button" name="btnEliminar" value="Eliminar">
-						<input type="button" name="btnConsultar" value="Consultar">
-					</td>
-					
-				</tr>
-				
-			</table>
+<form name="frmDetallesServicio">
 		
-		</form>
-	</body>
-
-</html>
-
+		<table border="1">
+			
+		<tr>
+		<td colspan="4" align="center"> DETALLE SERVICIO </td>					
+		</tr>
+				
+		<tr>
+		<td> CODIGO DETALLE: </td>
+		<td> <input type="text" name="txtcodigodetalle" size="8" maxlength="6"> </td>
+		</tr>	
+		<tr>
+		<td> FECHA ATENCION: </td>
+		<input type="text" name="txtfechaatencion" size="8" maxlength="4"> </td>
+				
+		</tr>
+		<tr>
+		<tr>
+		<td> DESCRIPCION: </td>
+		<td> <input type="text" name="txtdescripcion" size="60" maxlength="4"> </td>
+				
+		</tr>
+		<tr>
+		<td colspan="4" align="center">
+		<input type="button" name="btnGuardar" value="Guardar" class="botones">
+		<input type="button" name="btnModificar" value="Modificar">
+		<input type="button" name="btnEliminar" value="Eliminar">
+		<input type="button" name="btnConsultar" value="Consultar">
+		</td>
+					
+		</tr>
+				
+		</table>
+		
+</form>
