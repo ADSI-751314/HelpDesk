516a7338153f19ab6267106a850858fa6b6991ac
SENA <SENA@10.17.22.134>
2015-09-08 19:53:48 -0500

    detalles servicio

---
+++ view/forms/frmDetallesServicios.php
@@ -0,0 +1,39 @@
+<form name="frmDetallesServicio">
+			<table border="4" class="color_fondo">
+				<tr>
+					<td colspan="10" class="titulos" align="center"> DETALLES SERVICIO </td>					
+				</tr>
+				<tr>
+				<tr>
+				<tr>
+				<tr>
+					
+					<td class="textos"> CODIGO DETALLE: </td>
+					<td> <input type="text" name="txtPlaca" size="10" maxlength="10"> </td>
+				</tr>
+				</tr>
+				</tr>
+				</tr>
+				<tr>
+					<td class="textos"> FECHA ATENCION: </td>
+					<td> <input type="text" name="txtModelo" size="8" maxlength="4"> </td>
+				
+				</tr>
+				<tr>
+				<tr>
+					<td class="textos"> DESCRIPCION: </td>
+					<td> <textarea name="txtDescripcion" rows ="5" cols="25"> </textarea> </td>
+				
+				</tr>
+				<tr>
+				<td colspan="4" align="center" bgcolor="blue">
+						<input type="button" name="btnGuardar" value="Guardar" class="botones">
+						<input type="button" name="btnModificar" value="Modificar">
+						<input type="button" name="btnEliminar" value="Eliminar">
+						<input type="button" name="btnConsultar" value="Consultar">
+					</td>
+					
+				</tr>
+				
+			</table>
+</form>
