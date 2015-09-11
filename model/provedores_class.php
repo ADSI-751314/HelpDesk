6a707a4c963d9f263b8644a4638b002298e32164
sena <sena@10.17.16.73>
2015-09-10 21:17:43 -0500

    se a actualizado la clase  provedores

--- model/provedores_class.php
+++ model/provedores_class.php
@@ -26,11 +26,81 @@ class provedores_class {
   }
     
     
-    public function guardar($pro_codigo,$pro_nombre, $pro_telefono,$pro_telefono,$pro_direccion,$pro_correo,$pro_direccion)
+    public function guardar($pro_codigo,$pro_nombre, $pro_telefono,$pro_direccion,$pro_correo,$pro_pagina_web)
     {
-       $sql= " insert into provedores values ('$pro_codigo','$pro_nombre', '$pro_telefono','$pro_telefono','$pro_direccion','$pro_correo','$pro_direccion')";
+       $sql= " insert into provedores values ('$pro_codigo','$pro_nombre', '$pro_telefono','$pro_direccion','$pro_correo','$pro_pagina_web')";
         
         
         $query = $this->conexion->ejecutarQuery($sql);
     }
+    
+     public function modificar($pro_codigo,$pro_nombre, $pro_telefono,$pro_direccion,$pro_correo,$pro_pagina_web)
+    {
+       $sql= " UPDATE provedores SET ('$pro_nombre', '$pro_telefono','$pro_direccion','$pro_correo','$pro_pagina_web') where pk_pro_codigo=".$pro_codigo."";
+        
+        
+        $query = $this->conexion->ejecutarQuery($sql);
+    }
+    
+    
+     public function consultar($pro_codigo)
+    {
+     
+				//CONEXION CON LA BASE DE DATOS 
+				
+				  include_once  ("conexion.php");
+					$objconex = new Conexion ();
+					$conecta = $objconex-> conectar();
+					
+					// CONSULTA CASE DE DATOS 
+					
+					$consulta_cliente="select pk_pro_documento,pro_nombres,pro_telefono,pro_direccion,pro_correo,pro_pagina_web from provedores";
+					
+					//ejecucion de la consulta
+					
+					$fuente_cliente =mysql_query($consulta_cliente,$conecta);
+					
+					//obtener la cantidad de registros
+					$cantidad_cliente = mysql_num_rows($fuente_cliente);
+				
+				
+				// contruir un ciclo que recorra los registros que valla desde cero hasta las cantidad de clientes
+				for($i=0;$i<$cantidad_cliente;$i++)
+				{
+				$registro_cliente = mysql_fetch_row($fuente_cliente);
+				
+				$contenido= "<tr>
+							<td> " . $registro_cliente[0]            . " </td>
+							<td> " . $registro_cliente[1]            . " </td>
+							<td> " . $registro_cliente[2]            . " </td>
+							<td> " . $registro_cliente[3]            . " </td>
+							<td> " . $registro_cliente[4]            . " </td>
+                                                        <td> " . $registro_cliente[4]            . " </td>
+							
+							</tr>";
+				
+				echo $contenido;
+				}
+				//fin del ciclo
+				
+				
+				
+				
+				
+		
+    }
+    
+    
+    
+     public function eliminar($pro_codigo)
+    {
+       $sql= " DELETE FROM provedores where pk_pro_codigo=".$pro_codigo."";
+        
+        
+        $query = $this->conexion->ejecutarQuery($sql);
+    }
+    
+    
+    
+    
 }
