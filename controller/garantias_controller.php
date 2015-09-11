fbd03adf10dc22a7f86d7f544a3948d1c320fcf6
carloseduardo <carloseduardo@10.17.16.197>
2015-09-10 21:14:08 -0500

    se organiza el controller de garantias. faltan detalles de visualizacion y conexion

--- controller/garantias_controller.php
+++ controller/garantias_controller.php
@@ -14,10 +14,14 @@ switch ($proceso) {
         }
         break;
     case("consultar"): {
+        
+            $garantias->consultar($gar_id, $gar_hardware, $gar_software, $gar_fecha1, $gar_fecha2, $gar_descripcion);
             echo'consultar';
         }
         break;
     case ("eliminar"): {
+        
+            $garantias->eliminar();
             echo 'eliminado';
         }
         break;
@@ -33,6 +37,20 @@ class garantias_controller {
    
     {
         $test = new garantias_class();        
-        $test->almacenar($gar_id);
+        $test->almacenar($gar_id,$gar_hardware,$gar_software,$gar_fecha1,$gar_fecha2,$gar_descripcion);
+    }
+    public  function consultar($gar_id,$gar_hardware,$gar_software,$gar_fecha1,$gar_fecha2,$gar_descripcion)
+    {
+        
+    }
+    public function eliminar ($gar_id)
+    {
+        $elimi = new garantias_class();
+        $elimi->eliminar($gar_codigo);
+    }
+    public function modificar($gar_id,$gar_hardware,$gar_software,$gar_fecha1,$gar_fecha2,$gar_descripcion)
+    {
+        $modifi = new garantias_class();
+        $modifi->modificar($gar_id,$gar_hardware,$gar_software,$gar_fecha1,$gar_fecha2,$gar_descripcion);
     }
 }
--- model/garantias_class.php
+++ model/garantias_class.php
@@ -10,9 +10,29 @@ class garantias_class {
            $this->conexion = new conexion_class();
            $this->conexion->conexion();
     }
-    public function almacenar($gar_codigo)
+    public function almacenar($gar_codigo,$gar_hardware,$gar_software,$gar_fecha1,$gar_fecha2,$gar_descripcion)
     {
-        $sql= "insert into garantias values ('$gar_codigo')";
+        $sql= "insert into garantias values ('$gar_codigo','$gar_hardware','$gar_software','$gar_fecha1','$gar_fecha2','$gar_descripcion')";
         $query = $this->conexion->ejecutarQuery($sql);
+    }
+    public function consultar($gar_codigo,$gar_hardware,$gar_software,$gar_fecha1,$gar_fecha2,$gar_descripcion)
+    {
+        $sql="select * from garantias";
+        $query = $this->conexion->ejecutarQuery($sql);
+        
+    }
+    public function eliminar($gar_codigo)
+    {
+        $sql="DELETE FROM garantias WHERE pk_gar_codigo = ('$pk_gar_codigo')";
+        $query = $this->conexion->ejecutarQuery($sql);        
+    }
+    public function modificar ($gar_codigo,$gar_hardware,$gar_software,$gar_fecha1,$gar_fecha2,$gar_descripcion)
+    {
+        $sql="UPDATE garantias SET gar_hardware = '".$gar_hardware."',
+                                   gar_software = '".$gar_software."',
+                                   gar_fecha_inicio= '".$gar_fecha1."',
+                                   gar_fecha_fin = '".$gar_fecha2."',
+                                   gar_descripcion = '".$gar_descripcion."'";
+                                    
     }
 }
