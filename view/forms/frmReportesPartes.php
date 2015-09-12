1173cb776f126101101d04f3694497e916725479
Sena <Sena@10.17.19.88>
2015-09-11 21:05:49 -0500

    Creacion formularios Reportes

---
+++ view/forms/frmReportesPartes.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+        
+    
+    <form name="frmReportesPartes " >
+        <table border="1px">
+            <tr >                   
+
+                <td colspan="4"  >PARTES QUE MAS FALLAS  </td> 
+                <br>             
+            </tr>
+			<tr>  
+                <td>FECHA INICIO </td>
+                <td><input type="date" name="FechaInicio"> </td>
+                <td>FECHA FIN </td>
+                <td><input type="date" name="FechaFin"> </td>
+            </tr>
+            
+            
+            <tr>    <td> <input type="submit" name="btnConsultar" value="Consultar"  class="boton"></td>         </tr>
+              
+                                    
+        </table>            
+        
+        
+        
+        
+    </form>
+
