94800fdd173ea1164eae38d33725ad899b10b811
jaarboledam <jaarboledam@UbuntuPC>
2015-09-16 00:23:08 -0500

    Se agrega alterClass en general.js y cambios en general.css

--- view/js/general.js
+++ view/js/general.js
@@ -18,19 +18,24 @@ $(document).ready(function (e) {
     });
     
     $(".menu").on("click",function (){
-        $(".menu").siblings("ul").slideUp(200);
-        $(".menu").removeClass("active-menu");
-        $(".menu").children("i:last-child").removeClass("fa-chevron-up");
-        $(".menu").children("i:last-child").addClass("fa-chevron-down");
         if (!$(this).hasClass("active-menu")){
-            $(this).children("i:last-child").removeClass("fa-chevron-down");
-            $(this).children("i:last-child").addClass("fa-chevron-up");
+            var objMenu = $(".menu");
+            objMenu.siblings("ul").slideUp(200);
+            objMenu.removeClass("active-menu");
+            alterClass(objMenu.children("i:last-child"),"fa-chevron-up","fa-chevron-down");
+            alterClass($(this).children("i:last-child"),"fa-chevron-down","fa-chevron-up")
             $(this).addClass("active-menu");
             $(this).siblings("ul").slideDown(200);
         }else{
+            alterClass($(this).children("i:last-child"),"fa-chevron-up","fa-chevron-down")
             $(this).removeClass("active-menu");
             $(this).siblings("ul").slideUp(200);
         }
+    });
+    
+    $(".submenu > li").on("click",function (){
+        $(".submenu > li").removeClass("active-sm");
+        $(this).addClass("active-sm");
     });
     
     //-----------------Control de efectos login-------------------
@@ -41,3 +46,11 @@ $(document).ready(function (e) {
         $(this).siblings(".fa").removeClass("active-fa");
     });
 });
+
+//obj: objeto al que se le alterara el atributo clase
+//reCl: clase que será eliminada
+//adCl: clase que reemplazará la anterior
+function alterClass(obj,reCl,adCl){
+    obj.removeClass(reCl);
+    obj.addClass(adCl);
+};
