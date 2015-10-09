1514452f492f5ae1e7606d20be4b9c24452bda88
Sena <Sena@10.17.16.212>
2015-10-08 21:27:11 -0500

    se actualiza metodos de clase

--- model/provedores_class.php
+++ model/provedores_class.php
@@ -12,7 +12,7 @@
  * @author sena
  */
 
-include 'HelpDesk/model/conexion_class.php';
+include '../../HelpDesk/model/conexion_class.php';
 
 class provedores_class {
     
@@ -28,7 +28,7 @@ class provedores_class {
     
     public function guardar($pro_codigo,$pro_nombre, $pro_telefono,$pro_direccion,$pro_correo,$pro_pagina_web)
     {
-       $sql= " insert into provedores values ('$pro_codigo','$pro_nombre', '$pro_telefono','$pro_direccion','$pro_correo','$pro_pagina_web')";
+       $sql= " insert into proveedores values ('$pro_codigo','$pro_nombre', '$pro_telefono','$pro_direccion','$pro_correo','$pro_pagina_web')";
         
         
         $query = $this->conexion->ejecutarQuery($sql);
@@ -36,7 +36,7 @@ class provedores_class {
     
      public function modificar($pro_codigo,$pro_nombre, $pro_telefono,$pro_direccion,$pro_correo,$pro_pagina_web)
     {
-       $sql= " UPDATE provedores SET ('$pro_nombre', '$pro_telefono','$pro_direccion','$pro_correo','$pro_pagina_web') where pk_pro_codigo=".$pro_codigo."";
+       $sql= " UPDATE proveedores SET pro_nombre='".$pro_nombre."', pro_telefono='".$pro_telefono."',pro_direccion='".$pro_direccion."',pro_correo='".$pro_correo."',pro_pagina_web='".$pro_pagina_web."' where pk_pro_codigo='".$pro_codigo."'";
         
         
         $query = $this->conexion->ejecutarQuery($sql);
@@ -48,17 +48,16 @@ class provedores_class {
      
 				//CONEXION CON LA BASE DE DATOS 
 				
-				  include_once  ("conexion.php");
-					$objconex = new Conexion ();
-					$conecta = $objconex-> conectar();
+				
 					
 					// CONSULTA CASE DE DATOS 
 					
-					$consulta_cliente="select pk_pro_documento,pro_nombres,pro_telefono,pro_direccion,pro_correo,pro_pagina_web from provedores";
+					$consulta_cliente="select pk_pro_codigo,pro_nombre,pro_telefono,pro_direccion,pro_correo,pro_pagina_web from proveedores  ";
 					
 					//ejecucion de la consulta
+					$fuente_cliente=$this->conexion->ejecutarQuery($consulta_cliente);
+                                        
 					
-					$fuente_cliente =mysql_query($consulta_cliente,$conecta);
 					
 					//obtener la cantidad de registros
 					$cantidad_cliente = mysql_num_rows($fuente_cliente);
@@ -69,15 +68,15 @@ class provedores_class {
 				{
 				$registro_cliente = mysql_fetch_row($fuente_cliente);
 				
-				$contenido= "<tr>
+				$contenido= "<table><tr>
 							<td> " . $registro_cliente[0]            . " </td>
 							<td> " . $registro_cliente[1]            . " </td>
 							<td> " . $registro_cliente[2]            . " </td>
 							<td> " . $registro_cliente[3]            . " </td>
 							<td> " . $registro_cliente[4]            . " </td>
-                                                        <td> " . $registro_cliente[4]            . " </td>
+                                                        <td> " . $registro_cliente[5]            . " </td>
 							
-							</tr>";
+							</tr></table>";
 				
 				echo $contenido;
 				}
@@ -94,7 +93,7 @@ class provedores_class {
     
      public function eliminar($pro_codigo)
     {
-       $sql= " DELETE FROM provedores where pk_pro_codigo=".$pro_codigo."";
+       $sql= " DELETE FROM proveedores where pk_pro_codigo=".$pro_codigo."";
         
         
         $query = $this->conexion->ejecutarQuery($sql);
