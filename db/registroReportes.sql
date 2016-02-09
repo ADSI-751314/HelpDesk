8df8614a3ddb3d863702b8405872a58092f3e812
Tele-Corduo <Tele-Corduo@10.17.18.175>
2016-02-08 21:09:33 -0500

    Se insertan mas datos en tablas permisos, dependencias,roles, usuarios .

--- db/registroReportes.sql
+++ db/registroReportes.sql
@@ -54,7 +54,61 @@ now(),
 2,
 2);
 
- 
+
+
+insert into PERMISOS(
+pk_per_codigo,
+per_tipo,
+per_descripcion
+) values (3,
+'Consultar',
+'Descripcion Permiso'
+);
+insert into DEPENDENCIAS(
+pk_dep_codigo,
+dep_nombre,
+dep_extension) values (
+3,
+'Empleado',
+'3457');
+
+insert into ROLES(
+pk_rol_codigo,
+rol_descripcion,
+fk_per_codigo) values(
+3,
+'descripcion del rol',
+3);
+insert into USUARIOS (
+pk_usu_codigo,
+usu_primer_nombre,
+usu_segundo_nombre,
+usu_primer_apellido,
+usu_segundo_apellido,
+usu_extension,
+usu_cargo,
+usu_correo,
+usu_username,               
+usu_password,
+usu_fecha_creacion,
+usu_fecha_modificacion,
+fk_rol_codigo,
+fk_dep_codigo)
+values (
+3,
+'Yuli',
+'Tatiana',
+'Gonzalez',
+'Gonzalez'
+,'3457'
+,'Empleada'
+,'jutago@une.net.co',
+'jutago',
+'jutago1',
+now(),
+now(),
+3,
+3);
 
 
 
