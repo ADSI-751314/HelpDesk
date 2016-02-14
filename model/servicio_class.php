aa8f3d64067382134cff4aedb6bd01c36d5d7465
metalnando <hmoreno1987@hotmail.com>
2016-02-14 18:24:11 -0500

    Se realizan las correcciones a los formularios de adicionar los servicios, y la adicion de cada uno a la plantilla principal del proyecto,

--- model/servicio_class.php
+++ model/servicio_class.php
@@ -11,7 +11,7 @@
  *
  * @author USUARIO
  */
-include '/helpdesk/model/conexion_class.php';
+include 'conexion_class.php';
 
 class servicio_class {
     //put your code here
@@ -19,13 +19,13 @@ class servicio_class {
     
     public function __construct() {
         $this->conexion=new conexion_class();
-        $this->conexion=  conexion;
+        $this->conexion->conexion();
         
     }
     
     public function guardar($pTic, $pFec, $pHor, $pDes, $pEst, $pPrio, $pFecMod, $pUsu, $pEqu)
     {
-        $sql="INSERT INTO servicios VALUES('$pTic','$pFec','$pHor','$pDes','$pEst','$pPrio','$pFecMod','$pUsu','$pEqu')";
+        $sql="INSERT INTO servicios VALUES($pTic,'$pFec','$pHor','$pDes','$pEst','$pPrio','$pFecMod','$pUsu','$pEqu')";
         
         $query=  $this->conexion->ejecutarQuery($sql);
     }
@@ -46,14 +46,15 @@ class servicio_class {
     
     public function consultar($pTic)
     {
-        include_once 'conexion.php';
+        include_once 'conexion_class.php';
         
         $objConexion= new conexion_class();
-        $objconecta->$objConexion->conexion;
+        $objConecta->$objConexion->conexion();
+        
         
         $sql="SELECT * FROM servicios WHERE pk_ser_ticket = $pTic";
         
-        $query=  mysql_query($sql,$objconecta);
+        $query=  mysql_query($sql,$objConecta);
         
         $numCampos=  mysql_num_rows($query);
         
