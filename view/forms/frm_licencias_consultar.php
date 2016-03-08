f2810a6d5eea02dd376445577536ee571fd7ce01
Teleinformatica 01 <Teleinformatica 01@Teleinformatica>
2016-03-07 20:08:51 -0500

    SE REALIZO PRUEBAS A LICENCIAS Y CAMBIOS AL FORMULARIO PARA MEJOR USABILIDAD

--- view/forms/frm_licencias_consultar.php
+++ view/forms/frm_licencias_consultar.php
@@ -1,47 +1,25 @@
+<form action="controller/licencias_controller.php" method="POST"  name="crear">
+    <table colspan="3"align="center">
+        <tr>
+            <td> 
+                FORMULARIO LICENCIAS <br> CONSULTAR LICENCIAS
+            </td>
+        </tr>
+    </table>
+    <table colspan="3"align="center">
+        <tr>
+            <td>Codigo Licencia</td> 
+            <td><input type="text" name="txtCodigo_Licencia" id="txtCodigo_Licencia" value="" size="50" maxlength="80"></td>
+        </tr>
 
-<form action="../../controller/licencias_controller.php?petition=consultar" method="POST"  name="frm_licencias_consultar">
-	<table colspan="3"align="center">
-		<tr>
-			<td> 
-				FORMULARIO LICENCIAS <br> CONSULTAR
-			</td>
-			
-		</tr>
-	</table>
-
-	<table colspan="3"align="center">
-		<tr>
-			<td>Codigo Licencia</td> 
-			<td><input type="text" name="txtCodigo_Licencia" id="txtCodigo_Licencia" value="" size="50" maxlength="80"></td>
-		</tr>
-		
-		<tr>
-			<td>Nombre Licencia</td> 
-			<td><input type="text" name="txtNombre_Licencia"  id="txtNombre_Licencia" value="" size="50" maxlength="80"></td>
-		</tr>
-		
-		<tr>
-			<td>Fecha Compra </td> 
-                        <td>  <input type="date" name="Fecha_Compra"> </td>
-                          
-		</tr>
-		<tr>
-			<td>Fecha Caducidad</td> 
-                        <td> <input type="date" name="Fecha_Caducidad "> </td>
-                        
-		</tr>
-		<tr>
-			<td>Precio </td> 
-			<td><input type="text" name="txtPrecio"  id="txtPrecio" value="" size="50" maxlength="80"></td>
-		</tr>
-		<tr>
-			<td>Cobertura </td> 
-			<td><input type="text" name="txtCobertura"  id="txtCobertura" value="" size="50" maxlength="80"></td>
-		</tr>
-		
-		<table colspan="3"align="center">
+        <tr>
+            <td>Nombre Licencia</td> 
+            <td><input type="text" name="txtNombre_Licencia"  id="txtNombre_Licencia" value="" size="50" maxlength="80"></td>
+        </tr>
+        
+        <table colspan="3"align="center">
 		<tr> 
-			<td>Estado </td>
+			<td>Seleccione en su consulta un Estado </td>
 			<td>
 			<select id="Estado" onChange="mostrarSeleccionEquipos()">
 			<option> Seleccione </option>
@@ -56,8 +34,23 @@
 		</tr>
 		<tr align="center">
 		
-		<td><input type="submit" name="btnConsultar" size="35" value="Consultar" class="textos" onClick="Consultar()"> </td>
+		<td><input type="submit" name="btnListar_Todo" size="35" value="Listar Todo" class="textos"> </td>
 		</tr>
 		</table>
-	</table>
+                
+                <table id="lic_consulta" class="table table-bordered">
+                 <tr>
+                  <td><strong>Codigo</strong></td>
+                  <td><strong>Nombre</strong></td>
+                  <td><strong>Fecha Compra</strong></td>
+                  <td><strong>Fecha Inicio</strong></td>
+                  <td><strong>Fecha Caducidad</strong></td>
+                  <td><strong>Precio</strong></td>
+                  <td><strong>Cobertura</strong></td>
+                 </tr>
+                 
+              </table>
+       
+    </table>
 </form>
+<script type="text/javascript" src="view/js/licencias.js"/>
