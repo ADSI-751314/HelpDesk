c010d18a246ab2a2ab6e0ee93ab2889d1b64d12b
Teleinformatica 01 <Teleinformatica 01@TELE-15>
2016-03-11 19:58:26 -0500

    clase  politicas modificacion2

--- model/politicas_class.php
+++ model/politicas_class.php
@@ -1,6 +1,6 @@
 <?php
 
-include 'HelpDesk/model/conexion_class.php';
+include '../../HelpDesk/model/conexion_class.php';
 
 class politicas_class 
 {
@@ -24,9 +24,46 @@ class politicas_class
     
      public function modificar($pol_codigo,$pol_descripcion)
     {
-       $sql= " UPDATE politicas SET ('$pol_codigo','$pol_descripcion') where pk_pol_codigo=".$pol_codigo."";
+       $sql= " 'UPDATE politicas SET pol_codigo='" . $pol_codigo . "'pol_descripcion'" . $pol_descripcion . "'";
         
         
         $query = $this->conexion->ejecutarQuery($sql);
     }
-  }
+    
+    public function consultar($pol_codigo)
+    {
+        //cnx bd
+        //consulta bd
+        
+        $consulta_cliente = "select pk_pol_codigo,pol_descripcion";
+        
+        $fuente_cliente = $this->conexion->ejecutarQuery($consulta_cliente);
+        
+        $cantidad_cliente = mysql_num_rows($fuente_cliente);
+        
+        for ($i = 0; $i < $cantidad_cliente; $i++)
+        {
+            $registro_cliente = mysql_fetch_row($fuente_cliente);
+
+            $contenido = "<tr>
+							<td> " . $registro_cliente[0] . " </td>
+							<td> " . $registro_cliente[1] . " </td>
+							<td> " . $registro_cliente[2] . " </td>
+							<td> " . $registro_cliente[3] . " </td>
+							<td> " . $registro_cliente[4] . " </td>
+                                                        <td> " . $registro_cliente[5] . " </td>
+							
+							</tr>";
+
+            echo $contenido;
+        }
+    }
+        public function eliminar($pol_codigo) 
+        {
+        $sql = " DELETE FROM politicas where pk_pol_codigo=" . $pol_codigo . "";
+
+
+        $query = $this->conexion->ejecutarQuery($sql);
+        }
+    }
+  
