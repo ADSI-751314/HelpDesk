4b36e2883ae7d02f2108204ade1b2090e30849cd
Teleinformatica 01 <Teleinformatica 01@TELE-15>
2016-03-11 20:07:03 -0500

    actualizacion frmpoliticas consultar

--- view/forms/frm_politicas_consultar.php
+++ view/forms/frm_politicas_consultar.php
@@ -4,12 +4,12 @@
             <td colspan="2"><h1> Politicas <h1></td>
                         </tr>
                         <tr> 
-                            <td><label for="txt_Codigo">Codigo Politica</label></td> 
-                            <td><input type="text"  name="txt_Codigo" values="Ingrese  el codigo"> </td> 
+                            <td><label for="txtCodigo">Codigo Politica</label></td> 
+                            <td><input type="text"  name="txtCodigo" values="Ingrese  el codigo"> </td> 
                         </tr>
                         <tr> 
                             <td><label for="txtDescripcion">Descripción de la politica</label></td> 
-                            <td><input type="text"  name="txt_Descripcion" values="">  </td> 
+                            <td><input type="text"  name="txtDescripcion" values="">  </td> 
                         </tr>
                         <tr> 
                             <td colspan="2"><input type="submit"  name="btnConsultar" value=" "></td> 
