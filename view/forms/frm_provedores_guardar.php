dd959057e5d02c20450f88a2baa61f615323326e
Teleinformatica 01 <Teleinformatica 01@Teleinformatica>
2016-03-12 21:19:05 -0500

    se a modificado las peticiones del los formularios atravez de ajax para optimizar la sin necesidad recarga de la pagina

--- view/forms/frm_provedores_guardar.php
+++ view/forms/frm_provedores_guardar.php
@@ -1,28 +1,28 @@
-
+<script type="text/javascript" src="view/js/proveedores.js"></script>
  <div class="col-md-6">
-<form action="controller/provedores_controller.php?petition=guardar" method="POST" name="frm_provedores_guardar">
+<form action="controller/provedores_controller.php" method="POST" name="guardar">
     <table class="table">
                         <tr>
                         <td colspan="2"><h1> Registro de Provedores<h1></td>
                         </tr>
                         <tr> 
-                            <td>Codigo Provedor</td> 
+                            <td>Codigo Proveedor</td> 
                             <td><input type="text"  name="txtPro_codigo" values="Ingrese  el codigo"> </td> 
                         </tr>
                         <tr> 
-                            <td>Nombre Provedor</td> 
+                            <td>Nombre Proveedor</td> 
                             <td><input type="text"  name="txtPro_nombre" values="">  </td> 
                         </tr>
                         <tr> 
-                            <td>Telefono Provedor</td> 
+                            <td>Telefono Proveedor</td> 
                             <td><input type="text"  name="txtPro_telefono" values=""></td> 
                         </tr>
                         <tr> 
-                            <td>Direccion Provedor</td> 
+                            <td>Direccion Proveedor</td> 
                             <td><input type="text"  name="txtPro_direccion" values=""></td>
                         </tr>
                         <tr> 
-                            <td>Correo Provedor</td> 
+                            <td>Correo Proveedor</td> 
                             <td><input type="text"  name="txtPro_correo" values=""></td> 
                         </tr>
                         <tr> 
@@ -30,7 +30,7 @@
                             <td><input type="text"  name="txtpagina_web" values=""> </td> 
                         </tr>
                         <tr> 
-                            <td colspan="2"><input type="submit"  name="btnguardar" value="guardar"  class="btn btn-lg btn-success"></td> 
+                            <td colspan="2"><input type="submit"  name="btnguardar" id="btnguardar" value="guardar"  class="btn btn-lg btn-success"></td> 
                         </tr>
     </table>
 </form>
