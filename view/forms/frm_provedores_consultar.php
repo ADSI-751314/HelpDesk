53c9d719a769044424188f4d6ab1a267f39cf80d
Teleinformatica 01 <Teleinformatica 01@Teleinformatica>
2016-04-05 21:57:03 -0500

    se a modificado  totalmente la estructura de la pagina usando div para crear el diseño  y se  implementa en funcionamiento el  campo  ingresar parametros de busqueda  como  el mismo funcionamiento del boton  buscar y se modifica el boton listar

--- view/forms/frm_provedores_consultar.php
+++ view/forms/frm_provedores_consultar.php
@@ -1,101 +1,29 @@
 
 <script type="text/javascript" src="view/js/proveedores.js"></script>   
-<div class="col-md-6">
-    <form action="controller/provedores_controller.php" method="POST" name="consultar">
+<div class="col-md-12">
+   
 
+        <div class="well"  >
+     
+          
+            <h1> Consultar Provedores<h1></td> 
 
-        <table class="table">
+                    <div class="ROW">
+                       
+                                 <a onclick="mostrar();"  name="btnconsultar" value="consultar"class="btn btn-lg btn-success">Listar Proveedores</a>
 
-            <tr> 
-                <td colspan="2"  >  <h1> Consultar Provedores<h1></td> 
+                              
+                                 <input type="text"  id="txtPro_parametro" values="Ingrese  el codigo" placeholder="Ingrese Busqueda" class=" col-xs-5  ">
+                                 <a onclick="mostrarParametro();"  name="btnBuscar" value="Buscar"class="btn btn-lg btn-success ">Buscar</a>
+                             
 
-                            </tr>
+                    </div>
 
-                            <tr> 
-                                <td  >   
+                  
+                         
 
+        </div>
 
-                                    <a onclick="mostrar();"  name="btnconsultar" value="consultar"class="btn btn-lg btn-success">Consultar</a>
+  <div id="ProveedoresF"></div>
 
-                                </td> 
-
-
-                                <td>   Buscar Proveedor
-                                    <input type="text"  name="txtConsulta" values="Ingrese  el codigo"> </td> 
-
-
-                            </tr>
-                            <tr> 
-                                <td>  Codigo Proveedor</td> 
-                                <td> <input type="text"  name="txtPro_codigo" values="Ingrese  el codigo"> </td> 
-                            </tr>
-
-
-                            <tr> 
-                                <td> Nombre Proveedor</td> 
-                                <td> <input type="text"  name="txtPro_nombre" values="">  </td> 
-                            </tr>
-
-                            <tr> 
-                                <td>   Telefono Proveedor</td> 
-                                <td> <input type="text"  name="txtPro_telefono" values=""> </td> 
-                            </tr>
-
-                            <tr> 
-                                <td> Direccion Proveedor</td> 
-                                <td> <input type="text"  name="txtPro_direccion" values="">  </td> 
-                            </tr>
-
-                            <tr> 
-                                <td>  Correo Proveedor</td> 
-                                <td>   <input type="text"  name="txtPro_correo" values="">  </td> 
-                            </tr>
-
-                            <tr> 
-                                <td> Pagina Web</td> 
-                                <td>  <input type="text"  name="txtpagina_web" values=""> </td> 
-                            </tr>
-                            </table>
-
-                            <div id="ProveedoresF"></div>
-                            <?php
-                            //include ('../../model/provedores_class.php');
-                            //$mostrar = new provedores_class();
-                            //$mostrar->consultar($pro_codigo);
-                            ?>
-
-
-
-
-
-                            </div >          
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                            </form>
-
-
-
-
+</div>
