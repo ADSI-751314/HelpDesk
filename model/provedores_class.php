9dd83775f84b0298f967e7f01bd127864b0f8f53
Teleinformatica 01 <Teleinformatica 01@Teleinformatica>
2016-04-07 21:27:42 -0500

    se crea nuevo metodo  para cargar los registros a modificar

--- model/provedores_class.php
+++ model/provedores_class.php
@@ -17,13 +17,107 @@ class provedores_class {
         if(!$query = $this->conexion->ejecutarQuery($sql)){
             echo 'error';
         }
+        
+             if ($query == '1') {
+        echo '<div class="alert alert-success alert-dismissable">
+        <button type="button" class="close" data-dismiss="alert">&times;</button>
+        <strong>¡Bien!</strong> Dato agregado Correctamente.
+        </div>';
+    } else {
+        echo '<div class="alert alert-warning alert-dismissable">
+        <button type="button" class="close" data-dismiss="alert">&times;</button>
+        <strong>¡UPS!</strong> Ha ocurrido un error.
+        </div>';
+    }
     }
 
+    
+    function cargarModificar($id) {
+  
+  
+    $sql = "select pk_pro_codigo,pro_nombre,pro_telefono,pro_direccion,pro_correo,pro_pagina_web from proveedores where pk_pro_codigo = $id";
+   
+     $query = $this->conexion->ejecutarQuery($sql);
+    if (!$result = $this->conexion->ejecutarQuery($sql)) {
+        echo $conexion->error;
+    }else{
+       
+         while ($row = mysqli_fetch_array($result)) {
+           $contenido = '  <table CLASS="table">
+                    
+                       <tr> 
+                           <td colspan="2"  >  <h1> Modificar  de Proveedores<h1></td> 
+     
+                    </tr>
+                    
+                    <tr> 
+                          <td colspan="2"  >   
+                            
+                              <a onclick="modificar();"  name="btnmodificar" value="modificar" class="btn btn-lg btn-success">Modificar</a>
+                             
+                          </td> 
+     
+                    </tr>
+                    <tr> 
+                        <td>   Codigo Proveedor</td> 
+                        <td> <input type="text"  id="txtPro_codigo" values="'.$row[0].'"> </td> 
+                    </tr>
+                  
+                    
+                    <tr> 
+                        <td>  Nombre Proveedor</td> 
+                        <td> <input type="text"  id="txtPro_nombre" values="'.$row[1].'">  </td> 
+                    </tr>
+                    
+                    <tr> 
+                        <td>  Telefono Proveedor</td> 
+                        <td> <input type="text"  id="txtPro_telefono" values="'.$row[2].'"> </td> 
+                    </tr>
+                    
+                    <tr> 
+                        <td>  Direccion Proveedor </td> 
+                        <td> <input type="text"  id="txtPro_direccion" values="'.$row[3].'">  </td> 
+                    </tr>
+                    
+                    <tr> 
+                        <td>  Correo Proveedor</td> 
+                        <td>   <input type="text"  id="txtPro_correo" values="'.$row[4].'">  </td> 
+                    </tr>
+                    
+                    <tr> 
+                        <td>  Pagina Web </td> 
+                        <td>  <input type="text"  id="txtpagina_web" values="'.$row[5].'"> </td> 
+                    </tr>
+                    </table>';
+           
+           
+           echo $contenido;
+         }
+    }
+}
+    
+    
+    
+    
+    
+    
     public function modificar($pro_codigo, $pro_nombre, $pro_telefono, $pro_direccion, $pro_correo, $pro_pagina_web) {
         $sql = " UPDATE proveedores SET pro_nombre='" . $pro_nombre . "', pro_telefono='" . $pro_telefono . "',pro_direccion='" . $pro_direccion . "',pro_correo='" . $pro_correo . "',pro_pagina_web='" . $pro_pagina_web . "' where pk_pro_codigo='" . $pro_codigo . "'";
 
 
         $query = $this->conexion->ejecutarQuery($sql);
+        
+         if ($query == '1') {
+        echo '<div class="alert alert-success alert-dismissable">
+        <button type="button" class="close" data-dismiss="alert">&times;</button>
+        <strong>¡Bien!</strong> Dato Modificado Correctamente.
+        </div>';
+    } else {
+        echo '<div class="alert alert-warning alert-dismissable">
+        <button type="button" class="close" data-dismiss="alert">&times;</button>
+        <strong>¡UPS!</strong> Ha ocurrido un error.
+        </div>';
+    }
     }
 
     public function consultar() {
@@ -44,6 +138,7 @@ class provedores_class {
                                     <td> DIRECCION </td>
                                     <td> CORREO</td>
                                     <td>PAGINA WEB</td>
+                                    <td>OPCIONES</td>
                                 </tr>';
         while ($row = mysqli_fetch_array($result)) {
             $contenido = "<tr>
@@ -53,7 +148,8 @@ class provedores_class {
 							<td> " . $row[3] . " </td>
 							<td> " . $row[4] . " </td>
                                                         <td> " . $row[5] . " </td>
-							
+							<td><a onclick='cargarModificar(' . $row[0] . ');'>Modificar</a><br><a onclick='eliminar(' . $row[0] . ');'>Eliminar</a></td>
+
 							</tr>";
 
             echo $contenido;
