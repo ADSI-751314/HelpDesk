7e8c2e44a6acbb929c7783c6e7c795c5c9cbd261
Teleinformatica 01 <Teleinformatica 01@Teleinformatica>
2016-04-07 19:28:26 -0500

    abril 7 2

--- view/forms/frm_politicas_consultar.php
+++ view/forms/frm_politicas_consultar.php
@@ -1,4 +1,4 @@
-<form action="HelpDesk/controller/politicas_controller.php?petition=consultar" method="POST" name="frm_politicas_consultar">
+<form action="controller/politicas_controller.php?petition=consultar" method="POST" name="frm_politicas_consultar">
     <table>
         <tr>
             <td colspan="2"><h1> Politicas <h1></td>
@@ -8,12 +8,12 @@
                             <td><input type="text"  name="txtCodigo" values="Ingrese  el codigo"> </td> 
                         </tr>
                         <tr> 
-                            <td><label for="txtDescripcion">Descripción de la politica</label></td> 
+                            <td><label for="txtDescripcion">Descripción de la politica mm</label></td> 
                             <td><input type="text"  name="txtDescripcion" values="">  </td> 
                         </tr>
                         <tr> 
                             <td colspan="4" aling="center">
-                            <input type="submit"  name="btnConsultar" value="guardar"></td> 
+                            <input type="submit"  name="btnConsultar" value="Consultar"></td> 
                         </tr>
                         </table>
  </form>
