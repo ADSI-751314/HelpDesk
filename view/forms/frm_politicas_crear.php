c5e2024d82b6f4db6ced3863b3f9e4eff36aac42
Teleinformatica 01 <Teleinformatica 01@Teleinformatica>
2016-04-08 20:22:29 -0500

    se elimina frmdetalleservicio esta en rpoceso

--- view/forms/frm_politicas_crear.php
+++ view/forms/frm_politicas_crear.php
@@ -12,7 +12,7 @@
             <td> <input type="text" name="txtCodigo" size="8" maxlength="6"> </td>
         </tr>
         <tr>
-            <td> DESCRIPCION mm: </td>
+            <td> DESCRIPCION: </td>
             <td> <textarea name="txtDescripcion" rows ="5" cols="25"> </textarea> </td>
 
         </tr>
