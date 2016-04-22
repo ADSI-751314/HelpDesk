be35aa7837a67d67e42a344ab5112cce3844ef4b
Teleinformatica 01 <Teleinformatica 01@Teleinformatica>
2016-04-21 20:03:42 -0500

    creacion frm detalles servicios eliminar

---
+++ view/forms/frm_detalles_servicios_eliminar.php
@@ -0,0 +1,15 @@
+<form action="controller/detalles_servicios_controller.php?petition=eliminar" method="POST" name="frm_detalles_servicios__eliminar">
+   <table>
+       <tr>
+           <td colspan="2"><h1> Eliminar Detalles Servicios <h1></td>
+                       </tr>
+                       <tr> 
+                           <td><label for="txtCodigo">Codigo Detalles Servicios</label></td>
+                           <td><input type="text"  name="txtCodigo" values="Ingrese  el codigo"> </td> 
+                       </tr>
+                       
+                       <tr> 
+                              <td><input type="submit" name="btnEliminar" size="35" value="Eliminar" class="textos" onClick="Eliminar()"> </td>
+                       </tr>
+                       </table>
+</form
