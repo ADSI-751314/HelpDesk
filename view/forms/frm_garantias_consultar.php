e9f824517fc39e054194ce29bf1a763300cd7823
Julian Arboleda <jaarboledam@gmail.com>
2016-05-08 00:47:51 -0500

    Mod: Garantias-Consultar: captura de eventos para los botones del formulario

--- view/forms/frm_garantias_consultar.php
+++ view/forms/frm_garantias_consultar.php
@@ -1,51 +1,48 @@
-<form action="/HelpDesk/controller/garantias_controller.php?petition=consultar" method="POST"  id="frmConsultar" name="consultar">
-            <br>
-             <h1 align="center"> FORMULARIO GARANTIAS (CONSULTAR)</h1>
-            <table border="2">
-                
+<form action="/HelpDesk/controller/garantias_controller.php?petition=consultar" method="POST" id="frmConsultar">
+    <br>
+    <h1 align="center"> FORMULARIO GARANTIAS (CONSULTAR)</h1>
+    <table border="2">
+        <tr>
+            <td> Número garantía a consultar:</td>
+            <td align="left"><input type="text" name="txtGarantiaID" id="numero_garantia" size="20" maxlength="12"></td>
+        </tr>
+        <tr>
+            <td>
+                <input type="submit" id="btnConsultar" name="btnConsultar" value="CONSULTAR" class="btn btn-info btn-submit">
+            </td>
+        </tr>
+    </table>
+
+    <div id="respuesta">
+        <table id="tabla" class="table table-hover">
+            <thead>
                 <tr>
-                    <td> Número garantía a consultar: </td>  <td align="left"><input type="text" name="txtGarantiaID" id="numero_garantia" size="20" maxlength="12"> </td>
+                    <td>Garantía Numero.</td>
+                    <td>Tipo de Garantía</td>
+                    <td>Fecha de Inicio garantía</td>
+                    <td>Fecha Fin de garantía</td>
+                    <td>Descripción</td>
+                    <td>Equipo</td>
                 </tr>
+            </thead>
+        </table>
+    </div>
+
+    <input type="button" name="btnlimpiar" value="LIMPIAR" class="btn btn-info" onclick="limpiarTexto()">
+    <input id="consultarT" type="button" name="btnconsulartT" value="consultar todo" class="btn btn-info btn-submit">
+
+    <div id="respuesta2">
+        <table id="tabla2" class="table table-hover">
+            <thead>
                 <tr>
-                    <td> <input type="submit" name="btnConsultar" value="CONSULTAR" class="btn btn-info"></td>
-                    
+                    <td>Garantía Numero.</td>
+                    <td>Tipo de Garantía</td>
+                    <td>Fecha de Inicio garantía</td>
+                    <td>Fecha Fin de garantía</td>
+                    <td>Descripción</td>
+                    <td>Equipo</td>
                 </tr>
-            </table>
-        
-             <div id="respuesta">
-            <table id="tabla" class="table table-hover">
-                <thead>
-                    <tr>
-                        <td>Garantía Numero.</td>
-                        <td>Tipo de Garantía</td>
-                        <td>Fecha de Inicio garantía</td>
-                        <td>Fecha Fin de garantía</td>
-                        <td>Descripción</td>
-                        <td>Equipo</td>                     
-                    </tr>
-                </thead>
-                
-            </table> 
-                
-          </div>
-              <input type="button" name="btnlimpiar" value="LIMPIAR" class="btn btn-info" onclick="limpiarTexto()">
-              <input id="consultarT" type="button" name="btnconsulartT" value="consultar todo" class="btn btn-info">
-              
-              <div id="respuesta2">
-            <table id="tabla2" class="table table-hover">
-                <thead>
-                    <tr>
-                        <td>Garantía Numero.</td>
-                        <td>Tipo de Garantía</td>
-                        <td>Fecha de Inicio garantía</td>
-                        <td>Fecha Fin de garantía</td>
-                        <td>Descripción</td>
-                        <td>Equipo</td>                     
-                    </tr>
-                </thead>
-                
-            </table> 
-                
-          </div>
+            </thead>
+        </table>
+    </div>
 </form>
-<script type="text/javascript" src="view/js/consultaGarantias.js"></script>
--- view/js/consultaGarantias.js
+++ view/js/consultaGarantias.js
@@ -1,85 +1,76 @@
-
 app.consultaGarantias = function () {
-    // funcion que se ejecuta cuando se hace submit en el form 
-    $("form").submit(function (e) {
-        //se cancela el evento del click para que no cambie de pagina.
-        e.preventDefault();
-        var controller = $(this).attr("action");
-        var method = $(this).attr("method");
-        var name = $(this).attr("name");
-        var data = $(this).serialize() + "&petition=" + name;
-        ajax(controller,data,method,respuesta);
+    $("#form-container").on('click', '.btn-submit', function (e) {
+        e.preventDefault();//se cancela el evento del click para que no cambie de pagina.
+        var $form = $('#frmConsultar');
+        var controller = $form.attr("action");
+        var method = $form.attr("method");
+        var name =  null;
+        var fn = null;
+
+        if (this.id === 'btnConsultar') {
+            name = 'consultar';
+            fn = respuesta;
+        } else {
+            name = 'consultar_todo';
+            fn = respuesta2;
+        }
+        var data = $form.serialize() + "&petition=" + name;
+
+        ajax(controller,data,method,fn);
     });
-    
-    $("#consultarT").on("click",function(){
-        var formulario = $("#frmConsultar");
-        var controller = formulario.attr("action");
-        var method = formulario.attr("method");
-        var name = "consultar_todo";
-        var data = formulario.serialize() + "&petition=" + name;
-        ajax(controller,data,method,respuesta2);
-    })
-};
 
-var tblBody = document.createElement("tbody");
-function respuesta(data) {
+    function respuesta(data) {
+        var tblBody = document.createElement("tbody");
 
-    data = JSON.parse(data);
-    var div = document.getElementById("respuesta");
-    var tabla = document.getElementById("tabla");
-    var hilera = null;
-    var celda = null;
+        data = JSON.parse(data);
+        var div = document.getElementById("respuesta");
+        var tabla = document.getElementById("tabla");
+        var hilera = null;
+        var celda = null;
 
-    for (var i = 0; i < 1; i++) {
+        for (var i = 0; i < 1; i++) {
+            hilera = document.createElement("tr");
 
-        hilera = document.createElement("tr");
-
-        for (var fila in data) {
-
-            celda = document.createElement("td");
-            var textoCelda = document.createTextNode(data[fila]);
-            celda.appendChild(textoCelda);
-            hilera.appendChild(celda);
-
+            for (var fila in data) {
+                celda = document.createElement("td");
+                var textoCelda = document.createTextNode(data[fila]);
+                celda.appendChild(textoCelda);
+                hilera.appendChild(celda);
+            }
+            tblBody.appendChild(hilera);
+            tabla.appendChild(tblBody);
+            div.appendChild(tabla);
         }
-        tblBody.appendChild(hilera);
-        tabla.appendChild(tblBody);
-        div.appendChild(tabla);
+    }
+
+    function respuesta2(data) {
+        var tblBody = document.createElement("tbody");
+
+        data = JSON.parse(data);
+        var div = document.getElementById("respuesta2");
+        var tabla = document.getElementById("tabla2");
+        var hilera = null;
+        var celda = null;
+
+        for (var i = 0; i < 1; i++) {
+            hilera = document.createElement("tr");
+
+            for (var fila in data) {
+                celda = document.createElement("td");
+                var textoCelda = document.createTextNode(data[fila]);
+                celda.appendChild(textoCelda);
+                hilera.appendChild(celda);
+            }
+
+            tblBody.appendChild(hilera);
+            tabla.appendChild(tblBody);
+            div.appendChild(tabla);
+        }
+    }
+
+    function limpiarTexto()
+    {
+         tblBody.innerHTML = "";
     }
 }
-
-var tblBody = document.createElement("tbody");
-function respuesta2(data) {
-
-    data = JSON.parse(data);
-    var div = document.getElementById("respuesta2");
-    var tabla = document.getElementById("tabla2");
-    var hilera = null;
-    var celda = null;
-
-    for (var i = 0; i < 1; i++) {
-
-        hilera = document.createElement("tr");
-
-        for (var fila in data) {
-
-            celda = document.createElement("td");
-            var textoCelda = document.createTextNode(data[fila]);
-            celda.appendChild(textoCelda);
-            hilera.appendChild(celda);
-
-        }
-        tblBody.appendChild(hilera);
-        tabla.appendChild(tblBody);
-        div.appendChild(tabla);
-    }
-}
-
-function limpiarTexto()
-{
-     tblBody.innerHTML = "";
-     
-}
-
-
 
