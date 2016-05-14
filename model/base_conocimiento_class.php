84774e8b7573154d78306e1363f722b1c8b994c7
Teleinformatica 01 <Teleinformatica 01@Teleinformatica>
2016-05-13 20:38:19 -0500

    Se TRABAJO EN formulario y al diseño de base de conocimiento

--- model/base_conocimiento_class.php
+++ model/base_conocimiento_class.php
@@ -3,21 +3,8 @@
 class base_conocimiento_class{
     
     private $conexion;
-    //TABLA DETALLE_SERVICIO 
-    private $pk_det_codigo;
-    private $det_descripcion;
-    private $det_fecha;
-   //TABLA HISTORIAL_CAMBIOS 
-    private $pk_htc_codigo;
-    private $htc_fecha;
-    private $htc_hora;
-    private $htc_descripcion;
-    //TABLA TEC_SERV_DET
-    private $pk_tsd_codigo;
-    private $fk_tec_codigo;
-    private $fk_ser_ticket;
-    private $fk_det_codigo;
-    //TABLA SERVICIOS 
+
+//TABLA SERVICIOS 
     private $pk_ser_ticket;
     private $ser_fecha;
     private $ser_hora;
@@ -27,7 +14,26 @@ class base_conocimiento_class{
     private $ser_fecha_modificacion;
     private $fk_usu_codigo;
     private $fk_equ_codigo;
-            
+                
+
+//TABLA DETALLE_SERVICIO 
+    private $pk_det_codigo;
+    private $det_descripcion;
+    private $det_fecha;
+  
+    
+//TABLA HISTORIAL_CAMBIOS 
+    private $pk_htc_codigo;
+    private $htc_fecha;
+    private $htc_hora;
+    private $htc_descripcion;
+   
+ //TABLA TEC_SERV_DET
+    private $pk_tsd_codigo;
+    private $fk_tec_codigo;
+    private $fk_ser_ticket;
+    private $fk_det_codigo;
+    
             
     public function __construct()
    {
@@ -38,26 +44,17 @@ class base_conocimiento_class{
     public function consultar(){
         
         
-        $sql="Select from detalles_servicios INNER JOIN tec_ser_det ON detalles_servicios.pk_det_codigo = tec_ser_det.fk_tsd_codigo'".$pk_det_codigo."' OR pk_tsd_codigo = '".$pk_tsd_codigo.'" OR pk_det_codigo ='".$det_descripcion.'";
+        $sql="select servicios.pk_ser_ticket,servicios.ser_descripcion,servicios.ser_fecha,
+detalles_servicios.det_descripcion,
+detalles_servicios.det_fecha,
+tec_serv_det.pk_tsd_codigo,
+tec_serv_det.fk_tec_codigo";
         
         $query = $this->conexion->consultarQuery($sql);
         return $query;
        
 
     }
-    interpretes inner join interprete_album
-on interpretes.pk_int_codigo = interprete_album.fk_int_codigo
-inner join albumes
-on interprete_album.fk_alb_codigo = albumes.pk_alb_codigo
-inner join canciones_albumes
-on albumes.pk_alb_codigo = canciones_albumes.fk_alb_codigo
-inner join canciones
-on  canciones_albumes.fk_can_codigo = canciones.pk_can_codigo
-inner join canciones_compositores
-on canciones.pk_can_codigo = canciones_compositores.fk_can_codigo
-inner join compositores
-on canciones_compositores.fk_com_codigo = compositores.pk_com_codigo
-
 
 
     public function consulta($pk_htc_codigo,$htc_fecha,$htc_hora,$htc_descripcion){
