f6ed54ee02ea2bf4f1062a244bc85d61def014b0
Teleinformatica 01 <Teleinformatica 01@Teleinformatica>
2016-05-13 21:11:07 -0500

    Se trabajo en la estructura de la consulta en Base de Conocimiento

--- model/base_conocimiento_class.php
+++ model/base_conocimiento_class.php
@@ -44,16 +44,28 @@ class base_conocimiento_class{
     public function consultar(){
         
         
-        $sql="select servicios.pk_ser_ticket,servicios.ser_descripcion,servicios.ser_fecha,
-detalles_servicios.det_descripcion,
-detalles_servicios.det_fecha,
-tec_serv_det.pk_tsd_codigo,
-tec_serv_det.fk_tec_codigo";
-        
+        $sql = "select servicios.pk_ser_ticket,servicios.ser_descripcion,servicios.ser_fecha,detalles_servicios.det_descripcion,detalles_servicios.det_fecha,
+            tec_serv_det.pk_tsd_codigo, tec_serv_det.fk_tec_codigo FROM 
+
+            servicios inner join tec_serv_det 
+            on servicios.pk_ser_ticket = tec_serv_det.fk_ser_ticket
+            inner join detalles_servicios 
+            on tec_serv_det.fk_det_codigo = detalles_servicios.pk_det_codigo
+            inner join equipos 
+            on servicios.fk_equ_codigo = equipos.pk_equ_codigo
+            inner join partes_equipos 
+            on equipos.pk_equ_codigo = partes_equipos.fk_equ_codigo
+            inner join historial_cambios
+            on partes_equipos.pk_pte_codigo = historial_cambios.pk_htc_codigo
+
+            where 
+
+            servicios.pk_ser_ticket like '%5%' or 
+            equipos.equ_nombre like '%com%' or 
+            partes_equipos.pte_descripcion like '%hardware%'";
+
         $query = $this->conexion->consultarQuery($sql);
         return $query;
-       
-
     }
 
 
