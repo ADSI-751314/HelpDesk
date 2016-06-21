39e517487890954fe262140aa4f8d816665f9369
Teleinformatica 01 <Teleinformatica 01@Teleinformatica>
2016-06-20 21:10:20 -0500

    se crea un espacio entre la barra de busqueda y los botones del menu

--- view/forms/frm_provedores_consultar.php
+++ view/forms/frm_provedores_consultar.php
@@ -12,6 +12,7 @@
                        
                         <li>   <input type="text"   id="txtPro_parametro" values="Ingrese  el codigo" placeholder="Ingrese Busqueda o de clic en la lupa y tendra todas las opciones" class="form-control" > </li> 
                       </ul>
+                   <br>
                         <ul class=" nav nav-justified"> 
                         <li>   <a onclick="mostrarParametro();"  name="btnBuscar" value="Buscar" class="btn btn-lg btn-primary"><i class="glyphicon glyphicon-search"></i></a> </li>
                           <li>    <a onclick="cargarAgregar();"  name="btnconsultar" value="Agregar"class="btn btn-lg btn-success" data-toggle="modal" data-target="#myModal">Agregar</a> </li>
