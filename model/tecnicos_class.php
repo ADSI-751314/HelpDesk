7dfbc47c3775c07b634879001c1bb069a64c2bf6
Teleinformatica 01 <Teleinformatica 01@10.17.18.175>
2016-06-21 20:05:51 -0500

    clase tecnicos ac tualizacion

--- model/tecnicos_class.php
+++ model/tecnicos_class.php
@@ -14,7 +14,7 @@ class tecnicos_class
         $this->conexion->conexion();
         
   }
-    public function guardar($tec_codigo,$tec_pimer_nombre,$tec_segundo_nombre,$tec_primer_apellido,$tec_segundo_apellido,$tec_numero_telefono,$tec_numero_movil,$tec_correo,$tec_especialidad)
+    public function guardar($tec_codigo,$tec_primer_nombre,$tec_segundo_nombre,$tec_primer_apellido,$tec_segundo_apellido,$tec_numero_telefono,$tec_numero_movil,$tec_correo,$tec_especialidad)
     {
        $sql= " insert into tecnicos values ('$tec_codigo','$tec_primer_nombre','$tec_segundo_nombre','$tec_primer_apellido','$tec_segundo_apellido','$tec_numero_telefono','$tec_numero_movil','$tec_correo','$tec_especialidad')";
         
